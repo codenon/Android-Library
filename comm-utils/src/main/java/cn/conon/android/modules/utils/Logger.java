e61dc80a76bdec65928eeedbbaaf9bd21dec628c
codenon <codenon@163.com>
2015-07-17 13:43:59 +0800

    comm-utils

--- comm-utils/src/main/java/cn/conon/android/modules/utils/Logger.java
+++ comm-utils/src/main/java/cn/conon/android/modules/utils/Logger.java
@@ -393,32 +393,9 @@ public class Logger {
     }
 
     private static String formatMessage(String format, Object... args) {
-        for (int i = 0; i < args.length; i++) {
-            if (args[i] instanceof String[]) {
-                args[i] = prettyArray((String[]) args[i]);
-            }
-        }
         return String.format(format, args);
     }
 
-    private static String prettyArray(String[] array) {
-        try {
-            if (array.length == 0) {
-                return "[]";
-            }
-            StringBuilder sb = new StringBuilder("[");
-            int len = array.length - 1;
-            for (int i = 0; i < len; i++) {
-                sb.append(array[i]);
-                sb.append(", ");
-            }
-            sb.append(array[len]);
-            sb.append("]");
-            return sb.toString();
-        } catch (Exception e) {
-            return "null";
-        }
-    }
 
     // -------------------------------------------------------------------------------------------
 
